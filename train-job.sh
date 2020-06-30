3c5941ce1c7162fbf387166ff09651e8652eac61
mehrad <mehrad@stanford.edu>
2020-06-29 18:42:06 -0700

    Option to provide train and eval languages for train scripts

--- train-job.sh
+++ train-job.sh
@@ -2,7 +2,7 @@
 
 . /opt/genie-toolkit/lib.sh
 
-parse_args "$0" "s3_bucket owner dataset_owner task_name project experiment dataset model load_from" "$@"
+parse_args "$0" "s3_bucket owner dataset_owner task_name project experiment dataset model load_from train_languages eval_languages" "$@"
 shift $n
 
 set -e
@@ -15,13 +15,28 @@ if ! test  ${load_from} = 'None' ; then
 	aws s3 sync ${load_from}/ "$modeldir"/ --exclude "iteration_*.pth" --exclude "*eval/*"  --exclude "*.log"
 fi
 
-aws s3 sync --exclude "synthetic*.txt" s3://${s3_bucket}/${dataset_owner}/dataset/${project}/${experiment}/${dataset} dataset/
+IFS='+' ; read -ra TLS <<<"$train_languages"; IFS=' '
+echo "${TLS[@]}"
+IFS='+'; read -ra ELS <<<"$eval_languages"; IFS=' '
+echo "${ELS[@]}"
 
 rm -fr "$modeldir/dataset"
-mkdir "$modeldir/dataset"
 rm -fr "$modeldir/cache"
 mkdir -p "$modeldir/cache"
-ln -s "$HOME/dataset" "$modeldir/dataset/almond"
+
+if [[ "$task_name" == *"multilingual"* ]] ; then
+  # only sync the files needed for this training
+  for lang in "${TLS[@]}" "${ELS[@]}" ; do
+    aws s3 sync s3://${s3_bucket}/${dataset_owner}/dataset/${project}/${experiment}/${dataset} ${HOME}/dataset/ --exclude "*" --include "*$lang*"
+  done
+  mkdir -p $modeldir/dataset/almond/multilingual
+  ln -s ${HOME}/dataset/* $modeldir/dataset/almond/multilingual
+else
+  aws s3 sync --exclude "synthetic*.txt" s3://${s3_bucket}/${dataset_owner}/dataset/${project}/${experiment}/${dataset} dataset/
+  mkdir -p $modeldir/dataset/almond/multilingual
+  ln -s ${HOME}/dataset/* $modeldir/dataset/almond
+fi
+
 ln -s $modeldir /home/genie-toolkit/current
 mkdir -p "/shared/tensorboard/${project}/${experiment}/${owner}/${model}"
 
@@ -31,6 +46,8 @@ mkdir -p "/shared/tensorboard/${project}/${experiment}/${owner}/${model}"
 #}
 #trap on_error ERR
 
+# print directory tree
+ls -R
 
 genienlp train \
   --data "$modeldir/dataset" \
@@ -43,6 +60,8 @@ genienlp train \
   --save_every 1000 \
   --log_every 100 \
   --val_every 1000 \
+  --train_languages $train_languages \
+  --eval_languages $eval_languages \
   --exist_ok \
   --skip_cache \
   "$@" 
