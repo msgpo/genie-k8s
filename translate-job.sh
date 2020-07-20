19008992da8cab9fba11a739cde4a1d2a0a5c22f
mehrad <mehrad@stanford.edu>
2020-07-19 21:14:16 -0700

    Allow for second round of translation on erred examples

--- translate-job.sh
+++ translate-job.sh
@@ -2,51 +2,80 @@
 
 . /opt/genie-toolkit/lib.sh
 
-parse_args "$0" "s3_bucket owner dataset_owner project experiment tgt_lang input_splits dlg_side process_translations input_splits ignore_context task_name" "$@"
+parse_args "$0" "s3_bucket owner dataset_owner project experiment model_name_or_path tgt_lang dlg_side do_translation process_translations process_english input_splits" "$@"
 shift $n
-
-if [ "$ignore_context" = true ] ; then
-  echo "ignoring context"
-fi
 
 set -e
 set -x
 
 export translation_arguments="$@"
 
-run_translation(){
-  # copy workdir makefiles over
-  aws s3 cp --recursive s3://${s3_bucket}/${owner}/workdir/${project} ./ --exclude "*" \
-   --include '*.mk' --include '*.config' --include '*/schema.tt' --include 'Makefile' --include '*.py' \
-    --include "*${experiment}/${dlg_side}/en/*" --include '*dlg-shared-parameter-datasets*' --include "*dataset-dialogs/${experiment}/${dlg_side}/*"
-  # generate english ref file if does not exists
-  IFS='+'; read -ra input_splits_array <<<"$input_splits" ; IFS=' '
-  for f in "${input_splits_array[@]}" ; do
-    if ! test -s ${experiment}/${dlg_side}/en/aug-en/unquoted-qpis-marian/$f.tsv; then
-      make -B geniedir=/opt/genie-toolkit all_names=$f experiment_dialog=${experiment}/${dlg_side} process_data_dialogs
-      aws s3 sync ./ s3://geniehai/${owner}/workdir/${project}/ --exclude '*' --include "*${experiment}/${dlg_side}*"
-    fi
-    genienlp run-paraphrase \
-      --id_column 0 --input_column 1 --gold_column 1 \
-      --input_file ${experiment}/${dlg_side}/en/aug-en/unquoted-qpis-marian/$f.tsv \
-      --output_file ${experiment}/${dlg_side}/marian/it/unquoted-qpis-translated/$f.tsv \
-      --model_name_or_path Helsinki-NLP/opus-mt-en-${tgt_lang} \
-      --output_example_ids_too \
-      --replace_qp \
-      --skip_heuristics \
-      --task translate \
-      $translation_arguments
-    aws s3 sync ./ s3://geniehai/${owner}/workdir/${project}/ --exclude '*' --include '*unquoted-qpis-translated*'
-    if ${process_translations} ; then
-      make -B geniedir=/opt/genie-toolkit all_names=$f experiment_dialog=${experiment}/${dlg_side} skip_translation=true batch_translate_dlg_marian_${tgt_lang}
-    fi
-  done
+translate() {
+  genienlp run-paraphrase \
+    --id_column 0 --input_column 1 --gold_column 1 \
+    --input_file ${experiment}/${dlg_side}/en/aug-en/unquoted-qpis-marian/$f.tsv \
+    --output_file ${experiment}/${dlg_side}/marian/${tgt_lang}/unquoted-qpis-translated/$f.tsv \
+    --model_name_or_path ${model_name_or_path} \
+    --tgt_lang ${tgt_lang} \
+    --output_example_ids_too \
+    --replace_qp \
+    --skip_heuristics \
+    --task translate \
+    --return_attentions \
+    $translation_arguments
 }
 
 
-run_translation
+translate_err(){
+    genienlp run-paraphrase \
+    --id_column 0 --input_column 1 --gold_column 1 \
+    --input_file ${experiment}/${dlg_side}/en/aug-en/unquoted-qpis-marian/"$f"_errors.tsv \
+    --output_file ${experiment}/${dlg_side}/marian/${tgt_lang}/unquoted-qpis-translated/"$f"_errors.tsv \
+    --model_name_or_path ${model_name_or_path} \
+    --tgt_lang ${tgt_lang} \
+    --output_example_ids_too \
+    --replace_qp \
+    --skip_heuristics \
+    --task translate \
+    --return_attentions \
+    $translation_arguments \
+    --temperature 0.6
+}
 
 
+main() {
+  # copy workdir makefiles over
+  aws s3 cp --recursive s3://${s3_bucket}/${owner}/workdir/${project} ./ --exclude "*" \
+    --include '*.mk' --include '*.config' --include '*/schema.tt' --include 'Makefile' --include '*.py' \
+    --include "*${experiment}/${dlg_side}/en/*" --include "*${experiment}/${dlg_side}/marian/${tgt_lang}/*" --include '*dlg-shared-parameter-datasets*' --include "*dataset-dialogs/${experiment}/${dlg_side}/*"
 
+  # generate english ref file if does not exist
+  IFS='+'; read -ra input_splits_array <<<"$input_splits"; IFS=' '
+  for f in "${input_splits_array[@]}"; do
+    if ${process_english}; then
+      make -B geniedir=/opt/genie-toolkit all_names=$f experiment_dialog=${experiment} process_data_dialogs || true
+      aws s3 sync ./ s3://geniehai/${owner}/workdir/${project}/ --exclude '*' --include "*${experiment}/${dlg_side}*"
+    fi
 
+    # translate
+    if ${do_translation}; then
+      translate
+      aws s3 sync ./ s3://geniehai/${owner}/workdir/${project}/ --exclude '*' --include '*unquoted-qpis-translated*'
+    fi
 
+    # further process the translated files
+    if ${process_translations}; then
+      make -B geniedir=/opt/genie-toolkit all_names=$f experiment_dialog=${experiment} skip_translation=true batch_translate_dlg_marian_${tgt_lang}
+      aws s3 sync ./ s3://geniehai/${owner}/workdir/${project}/ --exclude '*' --include "*${experiment}/${dlg_side}*"
+    fi
+
+    # translate examples failed in previous rounds with more conservative temperatures (=0.6)
+    if ${retranslate_erred_examples}; then
+      make -B geniedir=/opt/genie-toolkit all_names=$f experiment_dialog=${experiment} skip_translation=true only_translate_erred_dlg_marian_${tgt_lang}
+      translate_err()
+    fi
+
+  done
+}
+
+main
--- translate.sh
+++ translate.sh
@@ -4,18 +4,19 @@
 . lib.sh
 
 
-parse_args "$0" "experiment input_splits tgt_lang=None dlg_side=user process_translations=false num_gpus=1 ignore_context=false " "$@"
+parse_args "$0" " experiment input_splits model_name_or_path=None tgt_lang=None dlg_side=user do_translation=false process_translations=false process_english=false num_gpus=1" "$@"
 shift $n
 check_config "S3_BUCKET OWNER DATASET_OWNER IMAGE PROJECT TRAIN_TASK_NAME"
 
 GPU_NUM=$num_gpus
 GPU_TYPE="p3.$((2*$num_gpus))xlarge"
 
-JOB_NAME=${OWNER}-translate-${experiment}
+JOB_NAME=${OWNER}-translate-${experiment}-${tgt_lang}
 cmdline="--s3_bucket ${S3_BUCKET} --owner ${OWNER} --dataset_owner ${DATASET_OWNER} --project ${PROJECT} --experiment $experiment \
          --input_splits $input_splits  --dlg_side $dlg_side \
-         --process_translations $process_translations --tgt_lang $tgt_lang \
-         --task_name ${TRAIN_TASK_NAME} --ignore_context ${ignore_context} -- "$(requote "$@")
+         --process_translations $process_translations --do_translation $do_translation --process_english $process_english \
+         --model_name_or_path $model_name_or_path --tgt_lang $tgt_lang \
+         -- "$(requote "$@")
 
 set -e
 set -x
