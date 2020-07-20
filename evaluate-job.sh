22143d12ad75e2396df8f918b967fe457037f179
mehrad <mehrad@stanford.edu>
2020-07-19 21:15:22 -0700

    Update evaluate scripts to be compatible with recent changes for translation

--- evaluate-job.sh
+++ evaluate-job.sh
@@ -2,7 +2,7 @@
 
 . /opt/genie-toolkit/lib.sh
 
-parse_args "$0" "s3_bucket owner project experiment model model_owner eval_set" "$@"
+parse_args "$0" "s3_bucket owner project experiment dataset model model_owner pred_languages pred_set_name" "$@"
 shift $n
 
 set -e
@@ -10,21 +10,34 @@ set -x
 
 # on_error () {
  	# on failure ship everything to s3
-# 	aws s3 sync . s3://almond-research/${owner}/models/${project}/${experiment}/${model}/failed_eval/
+# 	aws s3 sync . s3://geniehai/${owner}/models/${project}/${experiment}/${model}/failed_eval/
 # }
 # trap on_error ERR
 
 pwd
-aws s3 sync s3://${s3_bucket}/${owner}/workdir/${project} .
-#mkdir -p ${experiment}/models
-#aws s3 sync --exclude 'iteration_*.pth' --exclude '*_optim.pth' s3://almond-research/${owner}/models/${project}/${experiment}/${model}/ ${experiment}/models/${model}/
+aws s3 cp --recursive  s3://${s3_bucket}/${owner}/workdir/${project} . --exclude "*" --include '*.mk' --include '*.config' --include '*/schema.tt' --include 'Makefile' --include '*database-map.tsv'
+
+
+IFS='+'; read -ra PLS <<<"$pred_languages"; IFS=' '
+echo "${PLS[@]}"
+
+mkdir -p ./dataset
+
+for lang in "${PLS[@]}"; do
+  aws s3 sync s3://geniehai/${owner}/dataset/${project}/${experiment}/${dataset} ./dataset/ --exclude "*eval/*"  --exclude "*train*" --exclude "*" --include "*$lang*" --include "*annotated.tsv"
+done
 
 ls -al
 mkdir -p tmp
 export GENIE_TOKENIZER_ADDRESS=tokenizer.default.svc.cluster.local:8888
 export TZ=America/Los_Angeles
-make geniedir=/opt/genie-toolkit experiment=$experiment eval_set=${eval_set} ${experiment}/${eval_set}/${model_owner}/${model}.{nlu,dialogue}.results
 
-for f in $experiment/${eval_set}/${model_owner}/${model}.{nlu,dialogue}.{results,debug} ; do
-	aws s3 cp $f s3://${s3_bucket}/${owner}/workdir/${project}/${experiment}/${eval_set}/${model_owner}/
+
+for lang in "${PLS[@]}"; do
+  mkdir -p $experiment/${pred_set_name}/${lang}
+  make -B geniedir=/opt/genie-toolkit experiment_dialog=$experiment experiment=$experiment pred_set_name=${pred_set_name} input_eval_server="./dataset/${lang}/${pred_set_name}.tsv" ${experiment}/${pred_set_name}/${model}.nlu.results
+  for f in $experiment/${pred_set_name}/${model}.nlu.{results,debug} ; do
+    aws s3 cp $f s3://${s3_bucket}/${owner}/workdir/${project}/${experiment}/${pred_set_name}/${lang}/
+  done
+  cat $experiment/${pred_set_name}/${model}.nlu.results
 done
--- evaluate.sh
+++ evaluate.sh
@@ -3,12 +3,12 @@
 . config
 . lib.sh
 
-parse_args "$0" "experiment model model_owner eval_set=eval" "$@"
+parse_args "$0" "experiment model dataset model_owner=${OWNER} pred_languages=en pred_set_name=eval" "$@"
 shift $n
 check_config "S3_BUCKET OWNER IMAGE PROJECT"
 
-JOB_NAME=${OWNER}-evaluate-${experiment}-${model_owner}-${model}
-cmdline="--s3_bucket ${S3_BUCKET} --owner ${OWNER} --project ${PROJECT} --experiment ${experiment} --model ${model} --model_owner ${model_owner} --eval_set ${eval_set} -- "$(requote "$@")
+JOB_NAME=${OWNER}-evaluate-"${experiment//_/-}"-${model}-"${pred_set_name//_/-}"
+cmdline="--s3_bucket ${S3_BUCKET} --owner ${OWNER} --project ${PROJECT} --experiment ${experiment} --dataset ${dataset} --model ${model} --model_owner ${model_owner} --pred_languages ${pred_languages} --pred_set_name ${pred_set_name} -- "$(requote "$@")
 
 set -e
 set -x
